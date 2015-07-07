4e499e562882eb467702fb97bd4584398fcb12e6
Kristin Beese <beese@purdue.edu>
2015-07-07 12:52:59 -0700

    experimenting with clicking on photos and having text appear

---
+++ myscripts.js
@@ -0,0 +1,33 @@
+var clickPic1 = function() {
+	var a = document.getElementById("about");
+	a.innerHTML = "Hey there! I'm Kristin and welcome to my website!";
+};
+
+var clickPic2 = function() {
+	var a = document.getElementById("code");
+	a.innerHTML = "hello";
+};
+
+var clickPic3 = function() {
+	alert("about!");
+	var a = document.getElementById("resume");
+	a.innerHTML = "hello";
+};
+
+var clickPic4 = function() {
+	alert("about!");
+	var a = document.getElementById("music");
+	a.innerHTML = "hello";
+};
+
+var clickPic5 = function() {
+	alert("about!");
+	var a = document.getElementById("contact");
+	a.innerHTML = "hello";
+};
+
+// <p>Hey there! I'm <a href = "2015-07-07.pdf">Kristin</a>, and welcome to my website!  </p>
+//
+//
+//				<p>So what brought you here?  Maybe you saw the link on my <a href = "https://instagram.com/kaybeezeee/">instagram</a>,  <a href = "https://twitter.com/kaybeezee">twitter</a>, or <a href = "https://www.linkedin.com/pub/kristin-beese/a4/424/901">linkedin</a>?  Maybe I begged you to take a look at it and now you are finally doing it?  Or maybe you are just creeping.  That's cool too.</p>
+//				<p>The point is that I am just happy to have you here!  So who even is Kristin?  Well I can answer your questions. (current work in progress) </p>
