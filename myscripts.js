15e9a37faa3bd1d07c854347d175a2afd2c1d69f
Kristin Beese <beese@purdue.edu>
2015-07-08 09:01:21 -0700

    sliding content

--- myscripts.js
+++ myscripts.js
@@ -6,28 +6,25 @@ var pic5Clicked = 0;
 
 var clickPic1 = function() {
 	if(pic1Clicked == 0) {
-		var a = document.getElementById("about").innerHTML;
-		document.getElementById("display").innerHTML = a;
-		setToZero();
+		slideBack();
+    	$( "#about" ).slideDown( "slow" );
 		pic1Clicked = 1;
 	}
 	else {
-		document.getElementById("display").innerHTML="";
+    	$( "#about" ).slideUp( "slow" );
 		pic1Clicked = 0;
 	}
 	
 };
 
 var clickPic2 = function() {
-
 	if(pic2Clicked == 0) {
-		var a = document.getElementById("code").innerHTML;
-		document.getElementById("display").innerHTML = a;
-		setToZero();
+		slideBack();
+    	$( "#code" ).slideDown( "slow" );
 		pic2Clicked = 1;
 	}
 	else {
-		document.getElementById("display").innerHTML="";
+    	$( "#code" ).slideUp( "slow" );
 		pic2Clicked = 0;
 	}
 };
@@ -39,45 +36,45 @@ var clickPic3 = function() {
 
 var clickPic4 = function() {
 	if(pic4Clicked == 0) {
-		var a = document.getElementById("music").innerHTML;
-		document.getElementById("display").innerHTML = a;
-		setToZero();
+    	slideBack();
+    	$( "#music" ).slideDown( "slow" );
 		pic4Clicked = 1;
 	}
 	else {
-		document.getElementById("display").innerHTML="";
+    	$( "#music" ).slideUp( "slow" );
 		pic4Clicked = 0;
 	}
 };
 
 var clickPic5 = function() {
 	if(pic5Clicked == 0) {
-		var a = document.getElementById("contact").innerHTML;
-		document.getElementById("display").innerHTML = a;
-		setToZero();
+    	slideBack();
+    	$( "#contact" ).slideDown( "slow" );
 		pic5Clicked = 1;
 	}
 	else {
-		document.getElementById("display").innerHTML="";
-		setToZero();
+    	$( "#contact" ).slideUp( "slow" );
+		pic5Clicked = 0;
 	}
 };
 
-var setToZero = function() {
-	pic1Clicked = 0;
-	pic2Clicked = 0;
-	pic3Clicked = 0;
-	pic4Clicked = 0;
-	pic5Clicked = 0;
+var slideBack = function() {
+	if (pic1Clicked == 1) {
+		pic1Clicked = 0;
+		$( "#about" ).slideUp( "slow" );
+	}
+	else if (pic2Clicked == 1) {
+		pic2Clicked = 0;
+		$( "#code" ).slideUp( "slow" );
+	}
+	else if (pic4Clicked == 1) {
+		pic4Clicked = 0;
+		$( "#music" ).slideUp( "slow" );
+	}
+	else if (pic5Clicked == 1) {
+		pic5Clicked = 0;
+		$( "#contact" ).slideUp( "slow" );
+	}
 }
 
 
-// $( "p" ).click(function() {
-//   $( this ).slideUp();
-// });
-
-// <p>Hey there! I'm <a href = "2015-07-07.pdf">Kristin</a>, and welcome to my website!  </p>
-//
-//
-//				<p>So what brought you here?  Maybe you saw the link on my <a href = "https://instagram.com/kaybeezeee/">instagram</a>,  <a href = "https://twitter.com/kaybeezee">twitter</a>, or <a href = "https://www.linkedin.com/pub/kristin-beese/a4/424/901">linkedin</a>?  Maybe I begged you to take a look at it and now you are finally doing it?  Or maybe you are just creeping.  That's cool too.</p>
-//				<p>The point is that I am just happy to have you here!  So who even is Kristin?  Well I can answer your questions. (current work in progress) </p>
